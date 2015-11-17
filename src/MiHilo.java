dcacc88f14b2a463a91837e360e4b5281b2ef34f
xuvy28 <jorge.salguero.dominguez@gmail.com>
2015-11-17 12:00:55 +0100

    Eliminado comentarios chorras

--- src/MiHilo.java
+++ src/MiHilo.java
@@ -1,16 +1,6 @@
 
 import java.io.Serializable;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Manu
- */
 public class MiHilo extends Thread implements Serializable{
     
         private int idHilo;
