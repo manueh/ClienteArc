9483dabc5f852c048a5454840ca5e2c55880184f
Manu <Manu@Manu-PC>
2015-11-17 12:17:59 +0100

    Cliente "bien"

--- src/ClienteARC.java
+++ src/ClienteARC.java
@@ -45,58 +45,6 @@ public class ClienteARC {
             }
         }
     }
-    /*public void obtenerCoordenadas (){
-        Random x = new Random();
-        Random y = new Random();
-        
-        Dimension screenSize =  Toolkit.getDefaultToolkit().getScreenSize();
-        
-        this.coordx = (x.nextDouble() * screenSize.width + 0);
-        this.coordy = (y.nextDouble() * screenSize.height + 0);
-        
-    }
-    
-   public void iniciarCliente(MiHilo h){
-        
-        try {
-            //Asignamos el identificador del hilo y un socket a escuchar el puerto de un host
-            so = new Socket(host, puerto);
-            
-            //Mensaje que vamos a recibir
-            entradatxt = new DataInputStream(new DataInputStream(so.getInputStream()));
-            
-            mensajetexto = new DataOutputStream(so.getOutputStream());
-            mensajetexto.writeUTF("A la espera proceso "+h.getIdHilo());
-            mensajetexto.flush();
-            
-            System.out.println("ENTRADATXT.READ = "+ entradatxt.readUTF());
-            
-            switch (entradatxt.readUTF()) {
-                case "Conexión creada, mantente a la espera.":
-                    try {
-                        System.out.println("Entro en espera");
-                        this.wait(2000);
-                    } catch (InterruptedException ex) {
-                        Logger.getLogger(ClienteARC.class.getName()).log(Level.SEVERE, null, ex);
-                    }   break;
-                case "Comenzar envio de coordenadas":
-                    System.out.println("Ha llegado el mensaje comenzar para el hilo "+ h.getIdHilo());
-                    mensaje = new ObjectOutputStream(so.getOutputStream());
-                    mensaje.writeObject(h);
-                    mensaje.flush();
-                    break;
-            }
-
-            //mensaje que vamos a enviar
-            mensajetexto = new DataOutputStream(so.getOutputStream());
-            
-            
-            //so.close();
-        } catch (IOException ex) {
-            System.out.println("ERROR INICIAR CLIENTE "+ h.getIdHilo());
-        }
-    }
-    */
  
     public static void main(String[] args) {
         ClienteARC c = new ClienteARC();
