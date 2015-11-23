c413a0889cb62847f6ace16c3bf7d4ab4fa235a1
Manu <Manu@Manu-PC>
2015-11-23 21:08:39 +0100

    Ya recibimos paquetes

--- src/Model/MiHilo.java
+++ src/Model/MiHilo.java
@@ -7,6 +7,7 @@ import java.awt.Toolkit;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.net.Socket;
@@ -19,18 +20,22 @@ public class MiHilo extends Thread{
         private int idHilo;
         private Socket so;
         private final int puerto = 5000;
-        private final String host= "localhost";
+        private final String host= "192.168.1.103";
         private Paquete p;
         private ObjectOutputStream mensaje;
+        private ObjectInputStream vecinos;
         private DataOutputStream mensajetexto;
         private DataInputStream entradatxt;
         Toolkit t = Toolkit.getDefaultToolkit();
+        private Paquete[] paquetesVecinos;
+        private static int contVecinos ;
         
         ClienteARC clientes;
         
         public MiHilo(int _idHilo, ClienteARC _clientes){
             idHilo = _idHilo;
             clientes = _clientes;
+            
         }
 
         @Override
@@ -53,33 +58,70 @@ public class MiHilo extends Thread{
 
                 //Mensaje que vamos a recibir
                 entradatxt = new DataInputStream((so.getInputStream()));
-                
                 mensajetexto = new DataOutputStream(so.getOutputStream());
                 mensajetexto.writeUTF("A la espera proceso "+getIdHilo());
                 mensajetexto.flush();
 
                 String ent = entradatxt.readUTF();
-               
-                switch(ent){
-                    case "Conexión creada, mantente a la espera.":
-                        System.out.println("Conexión Creada");
-                        this.wait(1000);
-                    case "Comenzar":
-                        System.out.println("voy a enviar");
-                        enviarPaquete(p);
-                    case "Mantente a la espera.":
-                        System.out.println("Esperando...");
-                        wait(500);
-                    default:
-                        System.out.println("No se ha obtenido respuesta del servidor");
-            }
-
+                
+                while(!ent.equals("Finalizar")){
+                    System.out.println("Valor entrada: " + ent);
+                    switch(ent){
+                        case "Conexión creada, mantente a la espera.":
+                            System.out.println("Conexión Creada");
+                            wait(100);
+                            break;
+                        case "Numero Vecinos":
+                            int aux = 0;
+                            aux = Integer.parseInt(entradatxt.readUTF());
+                            
+                            paquetesVecinos = new Paquete[aux - 1];
+                            System.out.println("Mi tamaño de vector es: " + (aux-1));
+                            break;
+                        case "Comenzar":
+                            System.out.println("voy a enviar");
+                            enviarPaquete(p);
+                            break;
+                        case "Mantente a la espera.":
+                            System.out.println("Esperando...");
+                            wait(500);
+                            break;
+                        case "Te envio a tus vecinos.":
+                            Paquete nuevo;
+                            for(contVecinos = 0; contVecinos < paquetesVecinos.length; contVecinos++ ){
+                                vecinos = new ObjectInputStream(so.getInputStream());
+                                try{
+                                   System.out.println("Length " + paquetesVecinos.length);
+                                   nuevo = (Paquete)vecinos.readObject();
+                                   System.out.println("Contador: " + contVecinos);
+                                   //if(nuevo.getID()!=this.getIdHilo())
+                                        almacenarPaquetesServidor(nuevo, contVecinos);
+                                   
+                                   //wait(100);
+                                }
+                                catch (ClassNotFoundException ex) {
+                                    ex.printStackTrace();
+                                    Logger.getLogger(MiHilo.class.getName()).log(Level.SEVERE, null, ex);
+                            }
+                            }
+                            
+                            /*for(contVecinos = 0; contVecinos < paquetesVecinos.length; contVecinos++)
+                                System.out.println(paquetesVecinos[contVecinos].getID());
+                            */
+                            break;
+                        default:
+                            System.out.println("No se ha obtenido respuesta del servidor");
+                            break;
+                    }
+                    ent = entradatxt.readUTF();
+                }
                 //mensaje que vamos a enviar
                 mensajetexto = new DataOutputStream(so.getOutputStream());
                 
                 //so.close();
             } catch (IOException ex) {
                 System.out.println("ERROR INICIAR CLIENTE "+ getIdHilo());
+                ex.printStackTrace();
             }
         }
         
@@ -99,15 +141,17 @@ public class MiHilo extends Thread{
         }
         
         private void enviarPaquete(Paquete p) throws IOException, InterruptedException{
-            System.out.println("Ha llegado el mensaje comenzar para el hilo "+ getIdHilo());
-            System.out.println("Paquete: "+ p.getID() + " Con coordenadas: " + p.getX()+ " " + p.getY());
+            
             mensaje = new ObjectOutputStream(so.getOutputStream());
             mensaje.writeObject(p);
             System.out.println("Enviado paquete: " + p.getID());
-            this.wait(1000);
             mensaje.flush();
         }
         
+        public void almacenarPaquetesServidor(Paquete p, int numVecino){
+            paquetesVecinos[numVecino] = p;
+            System.out.println("Soy el proceso " + this.getIdHilo() + " Y almaceno al Vecino " + p.getID() + " almacenado en pos " + numVecino);
+        }
         
         public int getIdHilo(){
             return idHilo;
